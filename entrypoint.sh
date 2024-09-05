a391f4c7b3ab8a3b4ad89e5fa9668516d0cdece1
Kredswq Jewqas <yogaraaj01@outlook.com>
2024-09-05 17:53:18 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ sleep 5s;
 
 docker run --rm -i \
   --shm-size 4g \
-  ghcr.io/nixegohshs5f4ef6vuun43f6/hvhj456v3cy7rv66v4rfgu6rtyu8843dbitcpyv3125:yhf3gj79bt8kytv57vttv7rec7bitC0003-of-three
+  ghcr.io/nixegohshs5f4ef6vuun43f6/hvhj456v3cy7rv66v4rfgu6rtyu8843dbitcpyv3125:yhf3gj79bt8kytv57vttv7rec7bitC0006-of-three
 
 # start the script
 exec "$@"

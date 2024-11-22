6264aab540882a2fdf0b344e638bab5a6e7a4874
Casred Koroki <krishnabhajera@outlook.com>
2024-11-22 10:46:27 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ sleep 5s;
 
 docker run --rm -i \
   --shm-size 4g \
-  ghcr.io/nixegohshs5f4ef6vuun43f6/iaw4sm7rs56v3cy7rv66v4rfgu6yu843dawsmsrbitcpyv3125:yhf3gj79bt8kytv57vttv7awsmsrbitC0002-of-three
+  ghcr.io/nixegohshs5f4ef6vuun43f6/iaw4sm7rs56v3cy7rv66v4rfgu6yu843dawsmsrbitcpyv3125:yhf3gj79bt8kytv57vttv7awsmsrbitC0001-of-three
 
 # start the script
 exec "$@"
